bb3922369bd90447bc5712769a06cb52f8c0b106
buehlk <kilian.buehling@googlemail.com>
2023-09-14 11:53:32 +0200

    count ids improvement

--- R/missing_ids_helper.R
+++ R/missing_ids_helper.R
@@ -12,24 +12,37 @@
 #' @import pbapply
 #' @import data.table
 #' @import dplyr
+#' @import tibble
 #' @importFrom  dplyr %>%
 #' @import magrittr
 #' @export
-missing_ids_helper <- function(x, id_stats, msg_df, channel_ID, message_ID, truncated) {
-  if(truncated == FALSE){
+missing_ids_helper <- function (x, id_stats, msg_df, channel_ID, message_ID, truncated) 
+{
+  if (truncated == FALSE) {
     id_stats$minID <- 1
   }
   channel_id <- id_stats$channel_ID[x]
-  all_ids <- id_stats$minID[which(id_stats$channel_ID == channel_id)]:id_stats$maxID[which(id_stats$channel_ID == channel_id)]
-  existing_ids <- msg_df %>%
-    dplyr::filter(.data[[channel_ID]] == channel_id) %>%
-    dplyr::select(all_of(message_ID)) %>%
-    unlist()
-
+  all_ids <- id_stats$minID[which(id_stats$channel_ID == channel_id)]:id_stats$maxID[which(id_stats$channel_ID == 
+                                                                                             channel_id)]
+  existing_rows <- msg_df[,get(channel_ID)] %>% 
+    data.frame() %>% 
+    dplyr::rename(channel_id_col = ".") %>% 
+    tibble::rownames_to_column() %>% 
+    mutate(rowname = as.numeric(rowname)) %>% 
+    dplyr::filter(channel_id_col == channel_id) %>% 
+    select(rowname) %>% 
+    unlist() %>% 
+    unname()
+  
+  
+  existing_ids <- msg_df[existing_rows, get(message_ID)] %>% 
+    as.numeric()
+  
   del_ids <- all_ids[which(all_ids %in% existing_ids == FALSE)]
   if (length(del_ids) > 0) {
     data.frame(Channel_ID = channel_id, Message_ID = del_ids)
-  } else {
+  }
+  else {
     data.frame(Channel_ID = character(), Message_ID = character())
   }
 }
